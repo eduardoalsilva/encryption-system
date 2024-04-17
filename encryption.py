996137b0692e14e05b496441b2fa56f691b5e0c3
esilva <eduardoalsilva45@gmail.com>
2024-04-17 13:16:25 -0300

    Modify encryption.py

--- encryption.py
+++ encryption.py
@@ -1,14 +1,32 @@
 import string
 
-def encrypt_text(text, shift):
-    alphabet = string.ascii_letters
+class EncryptionSystem:
+    def __init__(self):
+        pass
 
-    encrypted_text = ''
-    for char in text:
-        if char not in alphabet:
-            encrypted_text += char
-            continue
-        index = alphabet.find(char)
-        encrypted_text += alphabet[(index + shift) % len(alphabet)]
+    def encrypt_text(text, shift):
+        alphabet = string.ascii_letters
 
-    return encrypted_text
+        encrypted_text = ''
+        for char in text:
+            if not char.isalpha():
+                encrypted_text += char
+                continue
+            index = alphabet.find(char)
+            encrypted_text += alphabet[(index + shift) % len(alphabet)]
+
+        return encrypted_text
+    
+    def decrypt_text(encrypted_text, original_shift):
+        alphabet = string.ascii_letters
+
+        decrypted_text = ''
+        for char in encrypted_text:
+            if not char.isalpha():
+                decrypted_text += char
+                continue
+
+            index = alphabet.find(char)
+            decrypted_text += alphabet[(index - original_shift) % len(alphabet)]
+
+        return decrypted_text
